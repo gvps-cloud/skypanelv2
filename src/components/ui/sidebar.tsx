62a984e313ae42189668e904f274fc2294b667eb
gvps-cloud <gvps-cloud@users.noreply.github.com>
2026-04-07 02:38:08 -0400

    fix(sidebar): adjust position of sidebar to bottom for better visibility

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -230,7 +230,7 @@ const Sidebar = React.forwardRef<
         />
         <div
           className={cn(
-            "fixed z-42 hidden h-svh w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear md:flex",
+            "fixed bottom-0 z-42 hidden w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear md:flex",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
